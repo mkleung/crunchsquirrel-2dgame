618cf240e84e54b63988f60c3b62761d9d16f5af
mike <mpakleung@gmail.com>
2019-05-16 16:17:06 -0400

    Add death and load scene

---
+++ Assets/Player_Health.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player_Health : MonoBehaviour
+{
+
+    public bool hasDied;
+    public int health;
+    // Start is called before the first frame update
+    void Start()
+    {
+        hasDied = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.y < -7) {
+            hasDied = true;
+        }
+
+        if (hasDied == true) {
+            StartCoroutine("Die");
+        }
+    }
+
+    // Restart the Scene
+    IEnumerator Die() {
+        SceneManager.LoadScene("Prototype1");
+        yield return null;
+    }
+}
--- Assets/Player_Move.cs
+++ Assets/Player_Move.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player_Move : MonoBehaviour
 {
-    public int playerSpeed = 10;
+    public int playerSpeed = 100;
     private bool facingRight = false;
     public int playerJumpPower = 1250;
 
