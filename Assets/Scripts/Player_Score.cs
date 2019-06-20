78c7a29480b13202838140e1b66f5cff4c9897ab
mike <mpakleung@gmail.com>
2019-06-20 00:17:49 -0400

    Refactoring

---
+++ Assets/Scripts/Player_Score.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Score : MonoBehaviour
+{
+
+    public float timeLeft = 120;
+    public int playerSore = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+    }
+}
